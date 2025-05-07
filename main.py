ad4bffca70866f9467948149a7bbbfb6cf5c7177
HamidReza <hamidrdeveloper@gmail.com>
2025-05-07 16:14:23 +0300

    first commit

--- main.py
+++ main.py
@@ -20,6 +20,38 @@ app.add_middleware(
 class RequestModel(BaseModel):
     siteUrl: str
     name: str
+class RequestModelID(BaseModel):
+    id: str
+    title: str
+@app.post("/api/OneNote/fetch-xml-all-id/")
+async def fetch_xml_all(request: RequestModelID):
+    # آدرس API مقصد
+    url = 'http://67.172.190.140:5108/api/OneNote/fetch-xml-all-id'
+    
+    # هدرها
+    headers = {
+        'Content-Type': 'application/json; charset=utf-8',
+        'sec-ch-ua-platform': 'Windows',
+        'Referer': 'https://kmt.solutions-apps.com/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0',
+        'sec-ch-ua': 'Chromium;v="136", "Microsoft Edge";v="136", "Not.A/Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'X-API-Key': 'your-api-key-here'
+    }
+    
+    # داده‌ها
+    data = {
+        "id": request.id,
+        "title": request.title
+    }
+    
+    # ارسال درخواست به API
+    try:
+        response = requests.post(url, json=data, headers=headers)
+        response.raise_for_status()
+        return JSONResponse(content=response.json(), status_code=response.status_code)
+    except requests.exceptions.RequestException as e:
+        return JSONResponse(content={"error": str(e)}, status_code=400)    
 
 @app.post("/api/OneNote/fetch-xml-all/")
 async def fetch_xml_all(request: RequestModel):
